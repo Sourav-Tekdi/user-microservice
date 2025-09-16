d8989e33b7c369a1c27e8dbb612d6a5fb80fa6a7
souravbhowmik1999 <sourav.bhowmik@tekditechnologies.com>
2025-09-16 16:45:14 +0530

    assign  api in interface

--- src/constants/routeconfig.js
+++ src/constants/routeconfig.js
@@ -391,19 +391,6 @@
 		]
 	  },
 	  {
-		"sourceRoute": "/interface/v1/academicyears/create",
-		"type": "POST",
-		"priority": "MUST_HAVE",
-		"inSequence": false,
-		"orchestrated": false,
-		"targetPackages": [
-		  {
-			"basePackageName": "user",
-			"packageName": "shiksha-user"
-		  }
-		]
-	  },
-	  {
 		"sourceRoute": "/interface/v1/cohortmember/bulkCreate",
 		"type": "POST",
 		"priority": "MUST_HAVE",
@@ -597,6 +584,58 @@
 			"packageName": "shiksha-user"
 		  }
 		]
-	  }
+	  },
+	  {
+		"sourceRoute": "/interface/v1/user/profile",
+		"type": "GET",
+		"priority": "MUST_HAVE",
+		"inSequence": false,
+		"orchestrated": false,
+		"targetPackages": [
+		  {
+			"basePackageName": "user",
+			"packageName": "shiksha-user"
+		  }
+		]
+	  },
+	  {
+		"sourceRoute": "/interface/v1/user/tenant/read/:id",
+		"type": "GET",
+		"priority": "MUST_HAVE",
+		"inSequence": false,
+		"orchestrated": false,
+		"targetPackages": [
+		  {
+			"basePackageName": "user",
+			"packageName": "shiksha-user"
+		  }
+		]
+	  },
+		{
+            "sourceRoute": "/interface/v1/user/sso/authenticate",
+            "type": "POST",
+            "priority": "MUST_HAVE",
+            "inSequence": false,
+            "orchestrated": false,
+            "targetPackages": [
+                {
+                    "basePackageName": "user",
+                    "packageName": "shiksha-user"
+                }
+            ]
+        },
+		{
+			"sourceRoute": "/interface/v1/user/hierarchical-search",
+			"type": "POST",
+			"priority": "MUST_HAVE",
+			"inSequence": false,
+			"orchestrated": false,
+			"targetPackages": [
+			  {
+				"basePackageName": "user",
+				"packageName": "shiksha-user"
+			  }
+			]
+		}
 	]
   }
